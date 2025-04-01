3cc99bb38050250eb3a687dc588334aab89e9d20
Kyle Speight <Electr_stream@outlook.com>
2025-03-31 21:39:47 -0700

    wow

--- script/js.js
+++ script/js.js
@@ -2,6 +2,5 @@ document.getElementById('minifyButton').addEventListener('click', function() {
     const jsInput = document.getElementById('jsInput').value;
     const minifiedJs = jsInput
     .replace(/\/\/.*|\/\*[\s\S]*?\*\//g, '')
-    .replace(/\s+/g, ' ')
-    .trim();
+    .replace(/\s+/g, ' ').trim();
     document.getElementById('minifiedOutput').value = minifiedJs;});
