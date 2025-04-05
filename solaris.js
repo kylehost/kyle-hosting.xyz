9da05670019e6f25d405b1c66c1c2f3e523605fc
Kyle Speight <Electr_stream@outlook.com>
2025-04-04 21:46:10 -0700

    Create solaris.js

---
+++ solaris.js
@@ -0,0 +1,13 @@
+// solaris.js
+(function() {
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-8TKSZ3WQ9E');
+})();
+
+var script = document.createElement('script');
+script.src = "https://www.googletagmanager.com/gtag/js?id=G-8TKSZ3WQ9E";
+script.async = true;
+document.head.appendChild(script);
